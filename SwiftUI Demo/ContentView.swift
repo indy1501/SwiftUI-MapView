54266a16b754781fe7f107abc244e41b16e923c8
indy1501 <indy.isb@gmail.com>
2020-03-28 18:56:09 -0700

    Initial Commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Maverick on 3/28/20.
+//  Copyright © 2020 Indrayani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
